0572197c017a21497b9a9b05bf871877cdaf92fd
LordSir101 <tywunderlich15@gmail.com>
2021-09-18 17:08:03 -0400

    add login function

---
+++ tucows_submit.py
@@ -0,0 +1,9 @@
+import selenium
+
+driver = webdriver.Chrome()
+driver.get('https://login.hrwize.com/login')
+
+# must be signed into google on your browser
+google_login_button = driver.find_element_by_name('google')
+google_login_button.click()
+
